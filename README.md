21138f158ce4832552f4fc4193f47bc7de92eeb9
iigmir <roc120j@gmail.com>
2017-07-13 17:13:26 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# 民國西元轉換器
+
+把民國年份轉換為西元的套件。
+
+## 版權宣告
+
+Icon image in icons/icon.png was made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>.
---
+++ popup/year.js
@@ -0,0 +1,32 @@
+document.querySelector('#year_calc').addEventListener("click", (e) => {
+    var year_ipt = {
+        unit : document.querySelector("#year_unit").value ,
+        type : parseInt( document.querySelector("#year_type").value , 10 )
+    };
+    var mingo_year = 1911;
+    var year_result = 0;
+    console.log(year_ipt);
+    if ( isNaN( year_ipt.type ) === true )
+    {
+        document.querySelector("#year_inputed").innerText = "Invaild input!";
+    }
+    else
+    {
+        document.querySelector("#year_inputed").innerText = year_ipt.type;
+        if ( year_ipt.unit == "mingo" )
+        {
+            year_result = year_ipt.type + mingo_year;
+            document.querySelector("#year_result").innerText = year_result;
+        }
+        else if ( year_ipt.unit == "grego" )
+        {
+            year_result = year_ipt.type - mingo_year;
+            document.querySelector("#year_result").innerText = year_result;
+        }
+        else
+        {
+            console.log( year_ipt.unit );
+            document.querySelector("#year_result").innerText = "Error!";
+        }
+    }
+});
