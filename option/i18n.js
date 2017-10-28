d732f7b8c9474d4c5c28ed7208d8574bbef72b9e
iigmir <roc120j@gmail.com>
2017-10-28 21:29:40 +0800

    Fix scrips part 1

---
+++ option/i18n.js
@@ -0,0 +1,14 @@
+function notify(message)
+{
+    console.log("background script received message");
+    var title = browser.i18n.getMessage("notificationTitle");
+    var content = browser.i18n.getMessage("notificationContent", message.url);
+    browser.notifications.create({
+        "type": "basic",
+        "iconUrl": browser.extension.getURL("icons/kmt.svg"),
+        "title": title,
+        "message": content
+    });
+}
+
+// browser.runtime.onMessage.addListener(notify);
--- option/option.js
+++ option/option.js
@@ -75,3 +75,25 @@ var en = {
     }
 };
 
+    "popup" : {
+        "year_unit" : {
+            "mingo" : "民國換算為西元",
+            "grego" : "西元換算為民國"
+        },
+        "year_calc" : "Convert year!",
+        "result": {
+            "year_inputed" : "你所輸入的年份：",
+            "year_result" : "換算出來的年份："
+        },
+    },
+    "option" : {
+        "default_format" : {
+            "name" : "預設轉換格式",
+            "m2g" : "民國至西元",
+            "g2m" : "西元至民國"
+        }
+    },
+    "error" : {
+        "invaild_input" : "輸入數值無效！",
+        "other" : "不明錯誤"
+    }
