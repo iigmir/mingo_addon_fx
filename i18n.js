29a2c448d89efe2547b0adb31ecb6e207ecb16ce
iigmir <roc120j@gmail.com>
2017-10-31 17:50:12 +0800

    Do fired jobs

---
+++ i18n.js
@@ -0,0 +1,8 @@
+function i18n(message)
+{
+    var exp = browser.i18n.getMessage("popup_msg_clac");
+    // popup_msg_clac
+    var i18n_array = ["popup_msg_m2g","popup_msg_g2m","popup_msg_clac","option_msg_name","error_msg_invalid_input","error_msg_invalid_other","result_inputed","result_outputed"];
+}
+
+browser.runtime.onStartup.addListener(notify);
