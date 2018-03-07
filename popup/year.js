8df46eb14831f5b02fe213fd6672b93a44a330a6
iigmir <roc120j@gmail.com>
2018-03-07 19:18:10 +0800

    Add Enter event

--- popup/year.js
+++ popup/year.js
@@ -3,6 +3,7 @@ var year_result = 0;
 
 function non_nan_detect( nd_input , result_shown_dom )
 {
+    debugger;
     var nd_invaild_message = "輸入無效";
     if ( isNaN( nd_input ) )
     {   // Show invaild message.
@@ -12,7 +13,8 @@ function non_nan_detect( nd_input , result_shown_dom )
     return true;
 }
 
-document.querySelector('#yat_calc').addEventListener("click", (e) => {
+function year_automatic_transfer()
+{
     var yat_input_year = parseInt( document.querySelector("#yat_type").value , 10 );
     var yat_vaild = non_nan_detect( yat_input_year , "#yat_result" );
     if( yat_vaild )
@@ -35,9 +37,10 @@ document.querySelector('#yat_calc').addEventListener("click", (e) => {
         document.querySelector("#yat_unit").innerText = yat_unit;
         document.querySelector("#yat_result").innerText = yat_result;
     }
-});
+}
 
-document.querySelector('#ymt_calc').addEventListener("click", (e) => {
+function year_manual_transfer()
+{
     var year_ipt = {
         unit : document.querySelector("#year_unit").value ,
         type : parseInt( document.querySelector("#year_type").value , 10 )
@@ -50,4 +53,22 @@ document.querySelector('#ymt_calc').addEventListener("click", (e) => {
         document.querySelector("#year_inputed").innerText = year_ipt.type;
         document.querySelector("#year_result").innerText = ymt_unit_vaild ? year_result : "單位無效！";
     }
+}
+
+document.querySelector('#yat_calc').addEventListener("click", year_automatic_transfer);
+document.querySelector('#ymt_calc').addEventListener("click", year_manual_transfer);
+document.addEventListener('keydown', (event) =>
+{
+    if ( event.keyCode === 13 )
+    {   // if enter pressed
+        if( document.querySelector("#ymt").hidden )
+        {   // Use AT instead of MT because it's hidden
+            year_automatic_transfer;
+        }
+        else
+        {   // Use MT instead of AT because it's hidden
+            year_manual_transfer;
+        }
+    }
+    return;
 });
